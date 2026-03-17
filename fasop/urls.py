2b2aa42d44ffa69db85b5b8f77581a425b5fd2f5
fasopup2b <harddanu@gmail.com>
2026-03-17 12:58:39 +0800

    update admin panel

--- fasop/urls.py
+++ fasop/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secure-panel/', admin.site.urls),
     path('', include('devices.urls')),
     path('maintenance/', include('maintenance.urls')),
     path('gangguan/', include('gangguan.urls')),
