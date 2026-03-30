b97fe2ad265ef11627906ef4fe626399efb60035
fasopup2b <harddanu@gmail.com>
2026-03-30 08:19:49 +0800

    update setting

--- fasop/settings.py
+++ fasop/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',')
 
 
 # Application definition
