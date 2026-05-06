7405783d9afad5876a83d81fba29bba42e64d7e6
binus <45899053+binusK2@users.noreply.github.com>
2026-05-06 13:52:40 +0800

    Increase max_length of status field to 30

--- dokumentasi/migrations/0004_settingrele_status_choices.py
+++ dokumentasi/migrations/0004_settingrele_status_choices.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
             model_name='settingrele',
             name='status',
             field=models.CharField(
-                max_length=22,
+                max_length=30,
                 choices=[
                     ('draft',           'Draft'),
                     ('on_check',        'On Check'),
